e83d53f90e5409cc96b811b70a3d82ebda33ff15
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-18 07:50:36 +0100

    Commit

--- src/Test.jsx
+++ src/Test.jsx
@@ -13,3 +13,15 @@ function SlowComponent() {
     </ul>
   );
 }
+
+function Counter({ children }) {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Slow counter?!?</h1>
+      <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+
+      {children}
+    </div>
+  );
+}
