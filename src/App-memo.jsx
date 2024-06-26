ae4f19a2474f12e1b177797a3d31430b05c342db
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-27 00:11:05 +0100

    Commit

--- src/App-memo.jsx
+++ src/App-memo.jsx
@@ -193,4 +193,8 @@ const Archive = memo(function Archive({ archiveOptions, onAddPost }) {
   );
 });
 
+function Footer() {
+  return <footer>&copy; by The Atomic Blog ✌️</footer>;
+}
+
 export default App;
