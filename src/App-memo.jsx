5bfe391c9c8be2cdba79e6cd0f4a0593d5df7704
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-27 00:10:19 +0100

    Commit

--- src/App-memo.jsx
+++ src/App-memo.jsx
@@ -148,4 +148,17 @@ function FormAddPost({ onAddPost }) {
   );
 }
 
+function List({ posts }) {
+  return (
+    <ul>
+      {posts.map((post, i) => (
+        <li key={i}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 export default App;
