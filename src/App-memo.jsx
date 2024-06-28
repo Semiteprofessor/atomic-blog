a172cc874aa1986714f0731eb01894de0d7b121f
Taiwo Olapade <semiteprofessor@gmail.com>
2024-06-29 00:06:18 +0100

    Commit

--- src/App-memo.jsx
+++ src/App-memo.jsx
@@ -87,4 +87,14 @@ function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {
   );
 }
 
+function SearchPosts({ searchQuery, setSearchQuery }) {
+  return (
+    <input
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      placeholder="Search posts..."
+    />
+  );
+}
+
 export default App;
