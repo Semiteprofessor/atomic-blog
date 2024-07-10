be2082217adec4796297fb1f1f5dedd4b5600643
Taiwo Olapade <semiteprofessor@gmail.com>
2024-07-10 23:59:52 +0100

    Commit

--- src/App-memo.jsx
+++ src/App-memo.jsx
@@ -1,4 +1,14 @@
 import { memo, useCallback, useEffect, useMemo, useState } from "react";
 import { faker } from "@faker-js/faker";
 
+
+
+function App() {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isFakeDark, setIsFakeDark] = useState(false);
+
+}
 export default App;
