9edb66e6311d6e40c2252dbefcc3f84d69d0bd1a
Taiwo Olapade <semiteprofessor@gmail.com>
2024-07-10 23:59:05 +0100

    Commit

--- src/App-memo.jsx
+++ src/App-memo.jsx
@@ -1,20 +1,4 @@
-import { Route, Routes } from "react-router-dom";
-import SinglePost from "./components/SinglePost";
-import { PostProvider } from "./ContextApi/PostContext";
-import "./App.css";
-import Landing from "./components/Landing";
-import NotFound from "./components/NotFound";
-
-function App() {
-  return (
-    <PostProvider>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/:id" element={<SinglePost />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </PostProvider>
-  );
-}
+import { memo, useCallback, useEffect, useMemo, useState } from "react";
+import { faker } from "@faker-js/faker";
 
 export default App;
