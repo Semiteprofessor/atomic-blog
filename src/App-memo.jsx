bf87a13033e29182864db6efa16eca2411c1e930
Taiwo Olapade <semiteprofessor@gmail.com>
2024-07-11 00:00:40 +0100

    Commit

--- src/App-memo.jsx
+++ src/App-memo.jsx
@@ -10,5 +10,24 @@ function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [isFakeDark, setIsFakeDark] = useState(false);
 
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  const handleAddPost = useCallback(function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }, []);
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+  const handleToggleFakeDark = useCallback(function handleToggleFakeDark() {
+    setIsFakeDark((prevIsFakeDark) =>!prevIsFakeDark);
+  }, []);
 }
 export default App;
