dd1f73f224e5430cc17d6c16e01c0155a376f8a5
Taiwo Olapade <semiteprofessor@gmail.com>
2024-07-11 23:56:20 +0100

    Commit

--- src/AppV1.jsx
+++ src/AppV1.jsx
@@ -155,4 +155,19 @@ function FormAddPost() {
   );
 }
 
+function List() {
+  const { posts } = useContext(PostContext);
+
+  return (
+    <ul>
+      {posts.map((post, i) => (
+        <li key={i}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 export default App;
