9036009d372d8eff47a0abdba2094feeda25deb6
Taiwo Olapade <semiteprofessor@gmail.com>
2024-07-19 23:52:24 +0100

    Commit

--- src/AppV1.jsx
+++ src/AppV1.jsx
@@ -90,5 +90,16 @@ function Header() {
   );
 }
 
+function SearchPosts() {
+  const { searchQuery, setSearchQuery } = useContext(PostContext);
+
+  return (
+    <input
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      placeholder="Search posts..."
+    />
+  );
+}
 
 export default App;
